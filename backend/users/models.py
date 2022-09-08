6e772fda174a4b1053ddaedc72363b05c2a9d51a
HyeonWooJo <HyeonWooJo@github.com>
2022-09-08 13:47:36 +0900

    Feat: 주문, 주문 상세, 결제 모델 구현

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+
 from core.models import TimeStampModel
 
+
 class User(TimeStampModel):
+    """
+    유저 모델
+    """
     email   = models.EmailField(max_length=120)
     psword  = models.CharField(max_length=100)
     name    = models.CharField(max_length=50)
