accb331fe1ddcb991a3858dabb1a9be07daa9bb4
HyeonWooJo <HyeonWooJo@github.com>
2022-09-08 15:57:05 +0900

    Feat: 댓글 모델 구현

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-from core.models import TimeStampModel
+from core.models     import TimeStampModel
+from products.models import Product
 
 
 class User(TimeStampModel):
@@ -17,3 +18,18 @@ class User(TimeStampModel):
 
     def __str__(self):
         return self.email
+
+
+class Comment(TimeStampModel):
+    """
+    댓글 모델
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user    = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200)
+    
+    class Meta:
+        db_table = 'comments'
+
+    def __str__(self):
+        return self.product
