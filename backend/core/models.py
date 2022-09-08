b478e83588c4aa36efc85d9751c2f273a9b340fb
HyeonWooJo <HyeonWooJo@github.com>
2022-09-08 13:21:53 +0900

    Feat: User 모델 구현

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+# 데이터 생성시간, 업데이트 시간 추상화 클래스
 class TimeStampModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from core.models import TimeStampModel
 
-# Create your models here.
+class User(TimeStampModel):
+    email   = models.EmailField(max_length=120)
+    psword  = models.CharField(max_length=100)
+    name    = models.CharField(max_length=50)
+    address = models.CharField(max_length=120)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.email
