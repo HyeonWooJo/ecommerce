52356b71fc1a2e85cc2757e8c84ba7a64f4cb403
HyeonWooJo <HyeonWooJo@github.com>
2022-09-13 09:32:12 +0900

    Feat: User 모델 변경

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,24 +1,56 @@
-from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db                     import models
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
 from core.models     import TimeStampModel
 from products.models import Product
 
 
-class User(TimeStampModel):
+class UserManager(BaseUserManager):
+    """유저매니저 정의"""
+
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_("Users must have an email address"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, TimeStampModel):
     """
     유저 모델
     """
-    email   = models.EmailField(max_length=120)
-    psword  = models.CharField(max_length=100)
-    name    = models.CharField(max_length=50)
-    address = models.CharField(max_length=120)
+    email    = models.EmailField(max_length=120)
+    username = models.CharField(max_length=50)
+    address  = models.CharField(max_length=120)
+    is_staff = models.BooleanField(_("Is staff"), default=False)
+
+    objects         = UserManager()
+    USERNAME_FIELD  = "email"
+    EMAIL_FIELD     = "email"
+    REQUIRED_FIELDS = []
 
     class Meta:
-        db_table = 'users'
+        verbose_name        = _("user")
+        verbose_name_plural = _("users")
+        db_table            = "users"
 
-    def __str__(self):
-        return self.email
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
 
 
 class Comment(TimeStampModel):
